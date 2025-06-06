6cf963dcfd2075b4c859f9c262aa9a3308d4773e
HARINARAYAN KUMAR <harinarayankumar806@gmail.com>
2025-06-06 17:58:18 +0530

    Update news_descriptions.py

--- news_descriptions.py
+++ news_descriptions.py
@@ -1,8 +1,8 @@
 # news_descriptions.py
 
 descriptions = {
-    "gdp-growth.jpg": "India's economy is projected to grow steadily due to strong domestic demand and government reforms.",
-    "imf-digital.jpg": "The IMF praised India's rapid digital progress including UPI, Aadhaar, and Digital India initiatives.",
-    "inflation.jpg": "The RBI has kept the repo rate unchanged, signaling inflation stability and economic confidence."
+    "gdp-growth.png": "India's economy is projected to grow steadily due to strong domestic demand and government reforms.",
+    "imf-digital.png": "The IMF praised India's rapid digital progress including UPI, Aadhaar, and Digital India initiatives.",
+    "inflation.png": "The RBI has kept the repo rate unchanged, signaling inflation stability and economic confidence."
 }
 
